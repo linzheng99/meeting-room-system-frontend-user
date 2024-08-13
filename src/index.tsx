431f0677b4c05e74f1e905160a51ef9eac3835a4
linzheng <alidlg9045@126.com>
2024-08-14 00:36:20 +0800

    feat: 用户端修改信息页面

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,26 @@ import { Register } from './page/register/Register';
 import { Login } from './page/login/Login';
 import { UpdatePassword } from './page/update_password/UpdatePassword';
 import { ErrorPage } from './page/error/ErrorPage';
+import { Index } from './page/index';
+import { UpdateInfo } from './page/update_info/UpdateInfo';
 
 const routes = [
   {
     path: "/",
-    element: <div>index</div>,
+    element: <Index />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'update_info',
+        element: <UpdateInfo />
+      },
+      {
+        path: 'bbb',
+        element: <div>bbb</div>
+      }
+    ]
   },
+
   {
     path: "login",
     element: <Login />,
