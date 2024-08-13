251fc70ad9b966972e049e57fc5cd367d65defe3
linzheng <alidlg9045@126.com>
2024-08-13 20:08:00 +0800

    用户注册模块

---
+++ src/insterface/interfaces.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { RegisterUser } from "../page/register/Register";
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000/',
+  timeout: 3000
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    console.log('axios response', response);
+    return response;
+  },
+  async (error) => {
+    return error.response;
+  }
+);
+
+
+export async function login(username: string, password: string) {
+  return await axiosInstance.post('/user/login', {
+    username, password
+  });
+}
+
+export async function registerCaptcha(email: string) {
+  return await axiosInstance.get('/user/register-captcha', {
+    params: {
+      address: email
+    }
+  });
+}
+
+export async function register(registerUser: RegisterUser) {
+  return await axiosInstance.post('/user/register', registerUser);
+}
